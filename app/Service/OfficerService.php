b1172a2bd205bf076ddc978aec119c7f46f85725
PuskarDaju <puskarniroula8@gmail.com>
2025-11-30 19:15:14 +0545

    minor fixes

--- app/Service/OfficerService.php
+++ app/Service/OfficerService.php
@@ -1,17 +1,21 @@
 <?php
+
 namespace App\Service;
 
 use App\Models\Appointment;
 use App\Models\Officer;
+use App\Models\WorkDay;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Facades\DB;
 
-class OfficerService{
+class OfficerService
+{
     protected AppointmentService $appointmentService;
 
-    public function __construct(){
-        $this->appointmentService=app(AppointmentService::class);
+    public function __construct()
+    {
+        $this->appointmentService = app(AppointmentService::class);
     }
 
     public function checkIfActive(int $id): bool
@@ -21,13 +25,17 @@ class OfficerService{
         return $officer && $officer->status === 'active';
     }
 
-    public function activateOfficer(int $id):bool
+    public function activateOfficer(int $id): bool
     {
         DB::beginTransaction();
 
         try {
             // Find the officer
             $officer = Officer::findOrFail($id);
+
+            if($officer->post->status=='inactive'){
+                return false;
+            }
 
             $now = Carbon::now('Asia/Kathmandu');
             // Get only future appointments
@@ -59,31 +67,41 @@ class OfficerService{
             return false;
         }
     }
-    public function deactivateOfficer(int $id):bool{
-      DB::beginTransaction();
-      try{
-          $now=Carbon::now();
-         $officer= Officer::findOrFail($id);
-          $appointments = Appointment::where('officer_id', $id)
-              ->where(function ($query) use ($now) {
-                  $query->where('appointment_date', '>', $now->toDateString())
-                      ->orWhere(function ($q) use ($now) {
-                          $q->where('appointment_date', $now->toDateString())
-                              ->where('start_time', '>=', $now->toTimeString());
-                      });
-              })
-              ->with('visitor')
-              ->get();
-          foreach ($appointments as $appointment){
-              $this->appointmentService->deactivateAppointment($appointment->id);
-          }
-    $officer->update(['status'=>'inactive']);
-          DB::commit();
-          return true;
-      }catch (\Exception $e){
-          DB::rollBack();
-          return false;
-      }
+
+    public function deactivateOfficer(int $id): bool
+    {
+        DB::beginTransaction();
+        try {
+            $now = Carbon::now();
+            $officer = Officer::findOrFail($id);
+            $appointments = Appointment::where('officer_id', $id)
+                ->where(function ($query) use ($now) {
+                    $query->where('appointment_date', '>', $now->toDateString())
+                        ->orWhere(function ($q) use ($now) {
+                            $q->where('appointment_date', $now->toDateString())
+                                ->where('start_time', '>=', $now->toTimeString());
+                        });
+                })
+                ->with('visitor')
+                ->get();
+            foreach ($appointments as $appointment) {
+                $this->appointmentService->deactivateAppointment($appointment->id);
+            }
+            $officer->update(['status' => 'inactive']);
+            DB::commit();
+            return true;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return false;
+        }
+    }
+
+    public function getWorkingOfficers()
+    {
+        return Officer::whereHas('workDay')->get();
+    }
+    public function getAllOfficers(){
+        return Officer::all();
     }
 
 }
