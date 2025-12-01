dd8bfd927972a35aa3f11e2fdb1df980598da497
PuskarDaju <puskarniroula8@gmail.com>
2025-12-01 13:05:22 +0545

    automatically marking the appointment to complete or cancelled

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Console\Commands\AutoCompleteRecords;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
